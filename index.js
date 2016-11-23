6d0fb2e685b6bddc7a29eb726e781dd458ee7675
slyguy <theslyguy@icloud.com>
2016-11-24 03:40:39 +0800

    final commit

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-var http = require("http");
-var express = require("express");
+var http = require("http"), express = require("express");
+var dateProcessor = require("./date-processor");
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set("view engine", "pug");
+app.use(express.static(__dirname + '/public'));
 
 app.get("/:datestr", function(req,res){
-    var datestr = req.param.datestr;
-    res.end("processing request" + datestr);          
+    var datestr = req.params.datestr;
+    console.log("Input date: " + datestr);
+    res.end(JSON.stringify(dateProcessor(datestr)));
 });
 
 app.get("/", function(req,res){
-    res.render('index', { title: 'Hey', message: 'Hello there!' });
+    res.render('index');
 });
 
 
